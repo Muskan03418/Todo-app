2349fc03e15a2146c5b7509df263db537d354d01
Muskan03418 <muskan3881.beai23@chitkara.edu.in>
2025-08-11 10:02:57 +0530

    Create todo.js

---
+++ todo.js
@@ -0,0 +1,108 @@
+
+let tasks = [];
+let taskid = 1;
+
+const taskName = document.querySelector("#taskName");
+const taskContainer = document.querySelector("#taskContainer");
+const addBtn = document.querySelector("#addBtn");
+addBtn.addEventListener("click", function () {
+    addDom();
+
+})
+
+taskName.addEventListener("keyup", function (e) {
+    // console.log(e);
+    if (e.key == "Enter") {
+        // tasks.push(taskName.value);
+        let task = {
+            id: taskid,
+            title: taskName.value,
+            status: "Pending"
+        }
+        tasks.push(task);
+        taskid++;
+
+
+        addDom(task);
+        taskName.value = "";
+    }
+
+
+})
+
+function addDom(task) {
+    let mainDiv = document.createElement("div");
+    mainDiv.setAttribute("id",task.id);
+    let titleSpan = document.createElement("span");
+    titleSpan.innerText = task.title;
+    mainDiv.appendChild(titleSpan);
+
+    let chkBox=document.createElement("input");
+    chkBox.setAttribute("type","checkbox");
+    chkBox.addEventListener("click",function(){
+       // console.log(chkBox.checked);
+       let status="Pending";
+        if(chkBox.checked)
+        {
+            status="Completed";
+            titleSpan.style.textDecoration="line-through";
+            //chkBox.remove();
+
+        }
+        else
+            titleSpan.style.textDecoration="none";
+
+        task.status=status;
+        console.log(tasks);
+    })
+    mainDiv.appendChild(chkBox);
+
+    let delBtn=document.createElement("button");
+    delBtn.innerText="Del";
+
+    delBtn.addEventListener("click",delhandler);
+    mainDiv.appendChild(delBtn);
+    
+
+    taskContainer.appendChild(mainDiv);
+    storetasks();
+    console.log(tasks);
+
+
+}
+
+
+function delhandler(e){
+   
+    let id=e.target.parentNode.getAttribute("id");
+    e.target.parentNode.remove();
+    console.log(id);
+    tasks=tasks.filter(function(item){
+        return item.id!=id;
+    })
+    console.log(tasks);
+    storetasks();
+}
+
+
+function storetasks(){
+    localStorage.setItem("tasks",JSON.stringify(tasks));
+
+}
+
+function loadtasks(){
+    if(localStorage.getItem("tasks"))
+   tasks=JSON.parse(localStorage.getItem("tasks"));
+   tasks.forEach(function(item){
+    if(taskid>item.id){
+        taskid=item.id;
+    }
+    addDom(item);
+
+   })
+   if(tasks.length!=0)
+        taskid++;
+
+}
+
+loadtasks();
